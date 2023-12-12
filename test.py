a0bcd6d0cd30bdf9a53b492407b4bf58b978e38d
unknown <stan.hordon@gmail.com>
2023-12-11 17:07:54 -0800

    task #1.  Надо пройти по этой ссылке (https://demoqa.com/automation-practice-form)   Перейти во вкладку Elements/Upload and Download   Загрузить любой файл со своего компа   Проверить что файл загрузился (там появится просто надпись типа "C:\fakepath\good tests.png")   Появишуюся надпись нужно скрапнуть и положить строку в лист my_file_paths

--- test.py
+++ test.py
@@ -1,38 +1,49 @@
 #! /usr/bin/env python3
-#from selenium import webdriver
+
 from selenium.webdriver.common.by import By
 import time
-from seleniumwire import webdriver
+from selenium import webdriver
 
 def open_website():
-    # Create a new browser instance
-    browser = webdriver.Chrome()
-
+        # Create a new browser instance
+    driver = webdriver.Chrome()
+        # Fullscrin browser
+    driver.maximize_window()
     # Open the website
-    browser.get("https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=ASKXGp3UMk4fMEAviIzGVzC-lNS7wM2JwfFCLO6sC4NH1Nn7iUaf7XAGd7Vhi5sbChZT8WTjE_7v6A&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-1257304344%3A1701982820617281&theme=glif")
+    driver.get("https://demoqa.com/automation-practice-form")
 
-    # Wait for the page to load
+        # Wait for the page to load
     time.sleep(2)  
+    
+        #open side bar element
+    side_bar_list = driver.find_elements(By.XPATH, '//span[@class="group-header"]')
+    #driver.execute_script("arguments[0].click();",upload_button) #javascipt click button all time
+    #print(f"THIS IS LENTH OF THE LIST{len(side_bar_list)}")
+    elements_button = side_bar_list[0]
+    elements_button.click()
+    time.sleep(3)
 
-    # username_input = webdriver.find_element(By.XPATH, '//input[@type="email"]')
-    username_input = browser.find_element(By.XPATH, '//input[@name="identifier"]')
+        # Scroll down 
+    driver.execute_script("window.scrollTo(0, 500);")
+        #click on Upload and download
+    upload_download_button = driver.find_element(By.XPATH, '//span[@class="text" and contains(text(), "Upload and Download")]')
+    time.sleep(2)  
+    upload_download_button.click()
+    time.sleep(3)
 
-    time.sleep(2)
-    # Вставляем логин в поле ввода
-    username_input.send_keys("dispatch.kimpho@gmail.com")
-    # for letter in "dispatch.kimpho@gmail.com":
-    #     username_input.send_keys(letter)
-    #     time.sleep(0.2)
-
-    time.sleep(5)
-    #push "next" button
-    next_button = browser.find_element(By.XPATH, '//button[@class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 qIypjc TrZEUc lw1w4b"]')
-    time.sleep(2)
-     # Click the next button
-    next_button.click()
-    time.sleep(20)
-    # Close the browser
-    browser.close()
+        #uploading file
+    upload_file = driver.find_element(By.XPATH, '//input[@id = "uploadFile"]')
+    upload_file.send_keys("C:/Users/stanh/Desktop/sampleFile.jpeg") #can't just click on button selenium do not supporting click on inpud button
+    time.sleep(3)
+    uploaded_file_path = driver.find_element(By.XPATH, '//input[@id="uploadFile"]').get_attribute("value")
+    my_file_paths = []
+    my_file_paths.append(uploaded_file_path)
+    print(my_file_paths)
+    return my_file_paths
+    
+    # # Close the browser
+    driver.close()
+    
 
 if __name__ == "__main__":
     open_website()
